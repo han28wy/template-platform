82b1c443bd3920f6b47996c77f3a10a0c743c2e3
hanxue <hanxue@cmhi.chinamobile.com>
2024-01-08 14:17:45 +0800

    OCT 配置多环境

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,8 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
 import { setupStore } from '@/stores';
 import './styles/index.scss';
-// import ElementPlus from 'element-plus';
-// import 'element-plus/dist/index.css';
 
 const app = createApp(App);
-// app.use(ElementPlus);
 
 dayjs.locale('zh-ch');
 setupStore(app);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
-import { VantResolver, ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import {
+  VantResolver,
+  ElementPlusResolver
+} from 'unplugin-vue-components/resolvers';
 import AutoImport from 'unplugin-auto-import/vite';
 import viteESLint from 'vite-plugin-eslint';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -10,28 +13,40 @@ const path = require('path');
 function resolve(dir: string) {
   return path.join(__dirname, dir);
 }
-// https://vitejs.dev/config/
-// https://github.com/vitejs/vite/issues/1930 .env
-export default defineConfig({
-  plugins: [
-    vue(),
-    viteESLint(),
-    AutoImport({
-      imports: ['vue', 'vue-router', 'pinia'],
-      dts: 'src/types/auto-import.d.ts', // 指明 .d.ts 文件的位置和文件名
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver(), VantResolver()]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': resolve('./src'),
-      '@common': resolve('./src/common'),
-      '@components': resolve('./src/components'),
-      '@store': resolve('./src/store'),
-      '@views': resolve('./src/views')
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(' jjj    ', env);
+  return {
+    plugins: [
+      vue(),
+      viteESLint(),
+      AutoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
+        dts: 'src/types/auto-import.d.ts', // 指明 .d.ts 文件的位置和文件名
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver(), VantResolver()]
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': resolve('./src'),
+        '@common': resolve('./src/common'),
+        '@components': resolve('./src/components'),
+        '@store': resolve('./src/store'),
+        '@views': resolve('./src/views')
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API_DOMAIN,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      }
     }
-  }
+  };
 });
